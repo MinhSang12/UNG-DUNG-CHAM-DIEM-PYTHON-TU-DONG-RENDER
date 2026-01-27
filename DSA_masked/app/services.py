46b9a40170d66e1beee02631656d0e72d67b9617
MinhSang <sanghuynh406@gmail.com>
2026-01-27 19:22:42 +0700

    Update Lastest 7h22 27/01

--- DSA_masked/app/services.py
+++ DSA_masked/app/services.py
@@ -1,9 +1,16 @@
 # app/services.py
 import requests
 from typing import Optional, Dict
+import os
 
 # URL trỏ đến API bạn vừa khởi chạy ở trên
-QUESTION_BANK_API_URL = "http://127.0.0.1:8000/db-api"
+RENDER_EXTERNAL_URL = os.getenv("RENDER_EXTERNAL_URL") 
+if RENDER_EXTERNAL_URL:
+    # URL trên Render (đã mount /db-api trong main.py)
+    QUESTION_BANK_API_URL = f"{RENDER_EXTERNAL_URL}/db-api"
+else:
+    # URL khi chạy ở máy cá nhân
+    QUESTION_BANK_API_URL = "http://127.0.0.1:8000/db-api"
 
 def fetch_problem_from_bank(topic_id: str) -> Optional[Dict]:
     if not topic_id: return None
